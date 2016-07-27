b106b28f4cace7877057f50137d5d471d1691f59
gilad1987 <gilad1987@gmail.com>
2016-07-27 17:11:09 +0300

    init

--- app/app.js
+++ app/app.js
@@ -20,7 +20,7 @@ import States     from 'states/states';
 // If you need to use lazy loading, you will probably need
 // to remove these two lines and explicitly require htmls
 const templates = require.context(__dirname, true, /\.html$/);
-const images = require.context(__dirname+'/assets/images', true, /\.jpg/);
+const images = require.context(__dirname+'/assets/images', true, /\.(jpg|png)/);
 const svgs = require.context(__dirname+'/assets/svgs', true, /\.svg/);
 
 templates.keys().forEach(templates);
